8ed9f268e5c910662354e80f711d80a3742d023b
longzuowei <daydayup0318@163.com>
2018-11-20 15:07:21 +0800

    react-route

---
+++ src/containers/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import RouterView from "router";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "comp/layout/header";
+
+import "common/css/style.css";
+
+class App extends React.Component {
+    render() {
+        return (<div className="wraper">
+            <Header></Header>
+            <section className="content">
+                <Router>
+                    <RouterView/>
+                </Router>
+            </section>
+        </div>)
+    }
+}
+
+export default App;
